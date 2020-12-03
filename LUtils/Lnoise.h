9ccbbc1176e5c16a79b008acb6b419a224500ba2
Erik Larsson <lrssn95@gmail.com>
2020-12-03 22:07:45 +0100

    Added Random

---
+++ LUtils/Lnoise.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace LUtils {
+	namespace LMath {
+		class LNoise {
+		public:
+			LNoise() {
+			};
+			int whitenoise();
+			
+		private:
+
+		};
+
+	}
+}
+
---
+++ LUtils/Random/LRandom.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "../pch.h"
+
+namespace LUtils {
+	class LRandomNumberGenerator {
+	public:
+		LRandomNumberGenerator() {
+			engine.seed(r);
+		};
+		LRandomNumberGenerator(unsigned int _seed) {
+			engine.seed(_seed);
+		};
+		bool fiftyfifty() {
+			std::uniform_int_distribution<> distrib(0, 1);
+			return distrib(engine);
+		}
+		int numberBetween(int _a, int _b) {
+			std::uniform_int_distribution<> distrib(_a, _b);
+			return distrib(engine);
+		}
+
+	private:
+		std::random_device r;
+		std::mt19937_64 engine;
+	};
+}
+
+
+
